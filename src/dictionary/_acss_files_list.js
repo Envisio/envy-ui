0f0d9d1a7344f747e1ca52b7e6a088bed4673155
Eugene Goncharov <egoncharov@envisio.com>
2020-04-07 16:54:43 -0700

    Add SG top right bottom left

--- src/dictionary/_acss_files_list.js
+++ src/dictionary/_acss_files_list.js
@@ -4,5 +4,6 @@ exports.acssFilesList = (pathToProp = '') => [
   `${pathToProp}acss/acss-padding.json`,
   `${pathToProp}acss/acss-width.json`,
   `${pathToProp}acss/acss-height.json`,
+  `${pathToProp}acss/acss-top-right-bottom-left.json`,
   `${pathToProp}acss/acss-display.json`,
 ];
