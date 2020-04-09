ad83973b9a58880bde43a1583f2ec1e6933903d0
Eugene Goncharov <egoncharov@envisio.com>
2020-04-09 08:18:57 -0700

    Add position and text topics to SG

--- src/dictionary/_acss_files_list.js
+++ src/dictionary/_acss_files_list.js
@@ -6,4 +6,6 @@ exports.acssFilesList = (pathToProp = '') => [
   `${pathToProp}acss/acss-height.json`,
   `${pathToProp}acss/acss-top-right-bottom-left.json`,
   `${pathToProp}acss/acss-display.json`,
+  `${pathToProp}acss/acss-position.json`,
+  `${pathToProp}acss/acss-text.json`,
 ];
