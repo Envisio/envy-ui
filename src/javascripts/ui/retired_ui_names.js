79da40b700b60cc58b61deafa7e0142233dd83a5
Eugene Goncharov <egoncharov@envisio.com>
2020-04-10 19:46:59 -0700

    Temporary keep junk

---
+++ src/javascripts/ui/retired_ui_names.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-multi-spaces */
+/* eslint-disable key-spacing */
+
+// General prefixes
+export const gp = 'rcpa-';
+export const gpOld = 'rc-pa-';
+export const gpNew = 'env-';
+
+export const uiGp = gpNew;
+
+export const $ui = {
+  // Abstract Library
+  A:                       `${gpNew}a-`,
+  // Forms
+  Form:                    `${gpNew}form`,
+  FormRow:                 `${gpNew}form-row`,
+  // Table
+  Table:                   `${gpNew}table`,
+  // Containers
+  Panel:                   `${gpNew}panel`,
+  WrapperScroll:           `${gp}scroll-wrapper`,
+  Info:                    `${gpNew}info`,
+  Card:                    `${gpNew}card`,
+  // Buttons & controls
+  Button:                  `${gpNew}button`,
+  ButtonSwitch:            `${gpNew}button-switch`,
+  Checkbox:                `${gpNew}checkbox`,
+  Calendar:                `${gpNew}calendar`,
+  // Inputs
+  InputCollapse:           `${gp}input-collapse`,
+  Collapser:               `${gpNew}collapser`,
+  InputText:               `${gpNew}input-text`,
+  Select:                  `${gpNew}select`,
+  InputGroup:              `${gpNew}input-group`,
+  // Menus
+  MenuDropdown:            `${gpNew}menu-dropdown`,
+  MenuFolder:              `${gp}menu-folder`,
+  // Others
+  Visual:                  `${gpNew}visual`,
+  // Abstracts with elements
+  ShowOnHover :            `${gpNew}show-on-hover`,
+  Focus :                  `${gpNew}focus`,
+  S3Uploader :             `${gpNew}s3-uploader`,
+  // Icons
+  IconStack :              `${gpNew}icon-stack`,
+  IconSvg :                `${gpNew}icon-svg`,
+  // Info Elements
+  Badge :                  `${gpNew}badge`,
+  Alert :                  `${gpNew}alert`,
+  // Loader
+  Loader :                  `${gpNew}loader`,
+  // Third party lib elements styling
+  AutosizeInput:           `${gpNew}autosize-input`,
+};
