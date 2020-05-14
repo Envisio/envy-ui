5a87505d4d07914251d7bce04335dd4346965c51
Eugene Goncharov <egoncharov@envisio.com>
2020-05-13 20:16:53 -0700

    Update styles for react select

--- src/javascripts/react_select/rs_dropdown_menu.js
+++ src/javascripts/react_select/rs_dropdown_menu.js
@@ -1,71 +1,74 @@
 import jssGlobal from '../from-css/ui-jss';
 import { UI_COLOR_PALE_BLUE } from '../index'
 
-
 const jss = jssGlobal['@global']
+// const jssCardMod = jss['.env-card--shadow-strong']
+// const jssButton = jss['.env-button']
 
-const jssCard = jss['.env-card']
-const jssCardMod = jss['.env-card--shadow-strong']
-const jssButton = jss['.env-button']
-
-export const rsDropdownMenu = ((jssCard, jssCardMod, jssButton, UI_COLOR_PALE_BLUE) => ({
-  styles: {
-    menu: (provided) => ({
-      ...provided,
-      width: 'auto',
-      minWidth: 'auto',
-      position: 'absolute',
-      display: 'flex',
-      marginTop: '0',
-      marginBottom: '0',
-      padding: '0',
-      right: '0',
-      ...jssCard,
-    }),
-    indicatorSeparator: (provided) => ({
-      ...provided,
-      display: 'none',
-    }),
-    valueContainer: (provided) => ({
-      ...provided,
-      border: '0',
-      maxWidth: '0',
-      padding: '0',
-      opacity: '0',
-    }),
-    control: (provided) => ({
-      ...provided,
-      borderWidth: '0',
-      boxShadow: '0',
-    }),
-    dropdownIndicator: (provided) => ({
-      ...provided,
-      padding: '0',
-    }),
-    menuList: (provided) => ({
-      ...provided,
-      display: 'flex',
-      flexDirection: 'column',
-      margin: '0',
-      padding: '0',
-      overflow: 'hidden',
-    }),
-    option: (provided) => ({
-      ...provided,
-    })
-  },
-  components: {
-    IndicatorSeparator: () => null,
-    Placeholder: () => null,
-    SingleValue: () => null,
-  },
-  theme: theme => ({
-    ...theme,
-    borderRadius: 3,
-    colors: {
-      ...theme.colors,
-      primary25: UI_COLOR_PALE_BLUE,
-      primary: UI_COLOR_PALE_BLUE,
-    },
-  }),
-}))(jssCard, jssCardMod, jssButton, UI_COLOR_PALE_BLUE);
+export const rsDropdownMenu = ({
+  jssCard: jss['.env-card'],
+  UI_COLOR_PALE_BLUE,
+  init: function () {
+    return {
+      styles: {
+        menu: (provided) => ({
+          ...provided,
+          width: 'auto',
+          minWidth: 'auto',
+          position: 'absolute',
+          display: 'flex',
+          marginTop: '0',
+          marginBottom: '0',
+          padding: '0',
+          right: '0',
+          ...this.jssCard,
+        }),
+        indicatorSeparator: (provided) => ({
+          ...provided,
+          display: 'none',
+        }),
+        valueContainer: (provided) => ({
+          ...provided,
+          border: '0',
+          maxWidth: '0',
+          padding: '0',
+          opacity: '0',
+        }),
+        control: (provided) => ({
+          ...provided,
+          borderWidth: '0',
+          boxShadow: '0',
+        }),
+        dropdownIndicator: (provided) => ({
+          ...provided,
+          padding: '0',
+        }),
+        menuList: (provided) => ({
+          ...provided,
+          display: 'flex',
+          flexDirection: 'column',
+          margin: '0',
+          padding: '0',
+          overflow: 'hidden',
+        }),
+        option: (provided) => ({
+          ...provided,
+        })
+      },
+      components: {
+        IndicatorSeparator: () => null,
+        Placeholder: () => null,
+        SingleValue: () => null,
+      },
+      theme: theme => ({
+        ...theme,
+        borderRadius: 3,
+        colors: {
+          ...theme.colors,
+          primary25: this.UI_COLOR_PALE_BLUE,
+          primary: this.UI_COLOR_PALE_BLUE,
+        },
+      }),
+    }
+  }
+}).init();
