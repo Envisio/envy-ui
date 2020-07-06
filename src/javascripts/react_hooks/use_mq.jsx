8f78a7cd1da12e8404e24c14131ee02b110aaab9
Eugene Goncharov <egoncharov@envisio.com>
2020-07-05 23:06:03 -0700

    Update react resize hook

--- src/javascripts/react_hooks/use_mq.jsx
+++ src/javascripts/react_hooks/use_mq.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import useResizeAware from 'react-resize-aware';
 
 export default function useMediaQuery({md = 0, lg = 99999}) {
-  // const [resizeListener, sizes] = useResizeAware();
-  // const { width } = sizes;
-  // const mq = {
-  //   sm: width <= md,
-  //   md: (width > md && width <= lg),
-  //   mdMinus: width <= lg,
-  //   mdPlus: width > md,
-  //   lg: width > lg,
-  // };
+  const [resizeListener, sizes] = useResizeAware();
+  const { width } = sizes;
+  const mq = {
+    sm: width <= md,
+    md: (width > md && width <= lg),
+    mdMinus: width <= lg,
+    mdPlus: width > md,
+    lg: width > lg,
+  };
 
-  // return [resizeListener, mq];
-  return [33, 44];
+  return [resizeListener, mq];
 };
