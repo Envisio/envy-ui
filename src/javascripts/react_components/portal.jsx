da19b5ba1ec03e0c0103029e743e6c4b4c247b7f
Eugene Goncharov <egoncharov@envisio.com>
2020-07-22 16:44:26 -0700

    Remove mount point

--- src/javascripts/react_components/portal.jsx
+++ src/javascripts/react_components/portal.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 const Portal = ({ children, domId }) => {
   const mount = document.querySelectorAll(domId)[0] || document.getElementsByTagName('body')[0];
-  const el = document.createElement('div');
+  // const el = document.createElement('div');
   // console.log(mount);
-  useEffect(() => {
-    mount.appendChild(el);
-    return () => mount.removeChild(el);
-  }, [el, mount]);
+  // useEffect(() => {
+  //   mount.appendChild(el);
+  //   return () => mount.removeChild(el);
+  // }, [el, mount]);
 
-  return createPortal(children, el);
+  return createPortal(children, mount);
 };
 
 Portal.propTypes = {
