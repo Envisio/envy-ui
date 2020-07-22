604dce6239d9859eb95b6f9931266172db2b85dd
Eugene Goncharov <egoncharov@envisio.com>
2020-07-21 20:34:47 -0700

    Add mount log

--- src/javascripts/react_components/portal.jsx
+++ src/javascripts/react_components/portal.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Portal = ({ children, domId }) => {
   const mount = document.querySelectorAll(domId)[0] || document.getElementsByTagName('body')[0];
   const el = document.createElement('div');
-  console.log(el);
+  console.log(mount);
   useEffect(() => {
     mount.appendChild(el);
     return () => mount.removeChild(el);
