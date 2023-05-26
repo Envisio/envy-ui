7c8b7ce4657760d6dbe4b7da8cfe0b750c9bebc4
Eugene Goncharov <egoncharov@envisio.com>
2023-05-26 10:55:26 -0700

    Update useMediaQuery log for testing

--- src/javascripts/react_hooks/use_mq.jsx
+++ src/javascripts/react_hooks/use_mq.jsx
@@ -2,7 +2,7 @@ import { useResizeDetector } from 'react-resize-detector';
 
 export default function useMediaQuery({md = 0, lg = 99999}) {
   const {width, ref} = useResizeDetector();
-  console.log('-----------------------------useMediaQuery resizeListener width', width);
+  console.log('-----------------------------useMediaQuery width', width);
   // console.log('useMediaQuery sizes', sizes);
   // const { width } = sizes;
   const mq = {
