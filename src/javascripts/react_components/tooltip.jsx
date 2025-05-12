39f70fc9fde1911fd65ecbc44170fbdb43b445ba
Eugene Goncharov <egoncharov@envisio.com>
2025-05-12 07:41:22 -0700

    Add tooltip component

--- src/javascripts/react_components/tooltip.jsx
+++ src/javascripts/react_components/tooltip.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
+import { styled } from '@mui/material/styles';
+import MuTooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import Portal from './portal';
+import { UI_COLOR_BORDER } from '../index';
 
 const Tooltip = ({ domId, ...props }) => {
 
@@ -16,6 +19,42 @@ const Tooltip = ({ domId, ...props }) => {
   );
 };
 
+
+// eslint-disable-next-line max-len
+const BootstrapTooltip = styled(({ className, maxWidth, ...props }) => <MuTooltip {...props} classes={{ popper: className }} />)(
+  ({ theme, maxWidth }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.common.black,
+      fontSize: '12px',
+      maxWidth,
+    },
+  }),
+);
+
+const BootstrapLightTooltip = styled(({ className, ...props }) => <MuTooltip {...props} classes={{ popper: className }} />)(({ theme }) => ({
+  // [`& .${tooltipClasses.arrow}`]: {
+  //   color: theme.palette.common.white,
+  // },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    fontSize: '12px',
+    border: `1px solid ${UI_COLOR_BORDER}`,
+  },
+}));
+
+export function EnvisioTooltip({ title = null, children, delay = false, isLight = false, ...props }) {
+  const CurrentTooltip = isLight ? BootstrapLightTooltip : BootstrapTooltip;
+  return (
+    <CurrentTooltip title={title || ''} enterDelay={delay ? 1500 : 100} enterNextDelay={delay ? 1500 : 0} {...props}>
+      {children}
+    </CurrentTooltip>
+  );
+}
+
+// eslint-disable-next-line react/forbid-prop-types
+EnvisioTooltip.propTypes = { children: PropTypes.object.isRequired, title: PropTypes.node, delay: PropTypes.bool, isLight: PropTypes.bool };
+
+
 Tooltip.propTypes = {
   domId: PropTypes.string,
 };
@@ -25,4 +64,6 @@ Tooltip.defaultProps = {
 };
 
 export default Tooltip;
+
+
 
